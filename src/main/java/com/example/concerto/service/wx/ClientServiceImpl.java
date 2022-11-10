133b66980dd5bb3cdff71661e3fdd7b6155dff3f
王炜嘉 <1920698385@qq.com>
2022-11-10 21:27:36 +0800

    写了一些service
    写了一些service

---
+++ src/main/java/com/example/concerto/service/wx/ClientServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.concerto.service.wx;
+
+import com.example.concerto.dao.ClientExpressDao;
+import com.example.concerto.dao.ExpressDao;
+import com.example.concerto.pojo.Client;
+import com.example.concerto.pojo.ClientExpress;
+import com.example.concerto.pojo.Express;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+    @Autowired
+    ExpressDao expressDao;
+    @Autowired
+    ClientExpressDao clientExpressDao;
+
+    @Override
+    public void insertExpress(Express express) {
+        expressDao.insertExpressByPojo(express);
+    }
+
+    @Override
+    public List<Express> queryExpressListByStatus(Client client, int status) {
+        List<Express> expressList = expressDao.getExpressListByTelAndStatus(client.getTel(), status);
+        return expressList;
+    }
+
+    @Override
+    public Express queryExpressByExpressNo(int expressNo) {
+        Express express = expressDao.getExpressByExpressNo(expressNo);
+        return express;
+    }
+
+    @Override
+    public void deleteExpressByExpressNo(int expressNo) {
+        Express express = new Express();
+        express.setExpressNo(expressNo);
+        expressDao.deleteExpressByPojo(express);
+    }
+}
